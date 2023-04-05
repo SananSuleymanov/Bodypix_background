427b6a5a13b03a289a701bfa4af90c947865e904
sasule <sasule@ttu.ee>
2023-04-05 20:16:46 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request, send_file
+import cv2
+import tensorflow as tf
+from tf_bodypix.api import load_model, download_model, BodyPixModelPaths
+
+import clip
+import numpy as np
+
+
+app= Flask(__name__)
+
+bp_model = load_model(download_model(BodyPixModelPaths.MOBILENET_FLOAT_50_STRIDE_16))
+
+@app.route('/', methods=['POST'])
+def remove():
+    image_path= request.args.get('image_path')
+    print(image_path)
+    image=cv2.imread(image_path)
+    prediction = bp_model.predict_single(image) 
+    mask = prediction.get_mask(threshold=0.05).numpy().astype(np.uint8)
+    new_mask = cv2.bitwise_and(image, image, mask=mask)
+    cv2.imwrite('output.jpg', new_mask)
+    if request.method=='POST':
+        return send_file('output.jpg', mimetype='image/jpeg')
+    else:
+        return send_file('output.jpg', mimetype='image/jpeg')
+
+
+if __name__=='__main__':
+    app.run(debug=True)
